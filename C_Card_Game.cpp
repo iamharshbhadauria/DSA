54a7eba8b9b0ffdc2e2ec0d6fa136093d4903f39
Harsh Bhadauria <97093305+iamharshbhadauria@users.noreply.github.com>
2023-10-07 22:26:57 +0530

    Add files via upload

---
+++ C_Card_Game.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+
+void solve(){
+    int n;
+    cin >> n;
+
+
+}
+
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    auto Harsh_Bhadauria = chrono::high_resolution_clock::now();
+
+    string s = "Armyinstitute";
+    int mn = 2 , mx = 7;
+reverse(s.begin() + mn, s.begin() + mx);
+cout << s ;
+}
