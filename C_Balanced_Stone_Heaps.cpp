730d49d95c335348b2ccf6329f01aabb88c97209
Harsh Bhadauria <97093305+iamharshbhadauria@users.noreply.github.com>
2023-10-09 23:59:32 +0530

    Add files via upload

---
+++ C_Balanced_Stone_Heaps.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+
+
+
+void dfs(int )
+
+void solve(){
+    int n , m;
+    cin >> n >> m;
+    char arr[n][m];
+    for(int i=0 ; i<n ; ++i) for(int j=0 ; j<m ; ++j) cin >> arr[i][j];
+
+
+
+
+}
+
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    solve();
+}
